efe4fca767d397c89caeaf4f03cb7a29aeaa372a
Andrew Copp <andrew.w.copp@gmail.com>
2015-06-26 01:16:48 -0400

    initial commit

---
+++ Manhattan/Classes/AppDependencies.swift
@@ -0,0 +1,25 @@
+//
+//  AppDependencies.swift
+//  Manhattan
+//
+//  Created by Andrew Copp on 6/26/15.
+//  Copyright © 2015 Andrew Copp. All rights reserved.
+//
+
+import Foundation
+
+class AppDependencies {
+    
+    
+    init() {
+       configureDependencies()
+    }
+    
+    func installRootViewControllerIntoWindow(window: UIWindow) {
+        
+    }
+    
+    func configureDependencies() {
+        
+    }
+}
